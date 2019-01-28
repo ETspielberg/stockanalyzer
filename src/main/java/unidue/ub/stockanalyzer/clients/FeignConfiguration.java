5cf1cd6f484539e8dded2646daba811397ee1ddf
Eike Spielberg <Eike.Spielberg@uni-due.de>
2019-01-28 10:36:20 +0100

    adding logging of requests

--- src/main/java/unidue/ub/stockanalyzer/clients/FeignConfiguration.java
+++ src/main/java/unidue/ub/stockanalyzer/clients/FeignConfiguration.java
@@ -1,6 +1,8 @@
 package unidue.ub.stockanalyzer.clients;
 
 import feign.auth.BasicAuthRequestInterceptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,8 +16,11 @@ public class FeignConfiguration {
     @Value("${libintel.system.password}")
     private String password;
 
+    private static Logger log = LoggerFactory.getLogger(FeignConfiguration.class);
+
     @Bean
     public BasicAuthRequestInterceptor basicAuthRequestInterceptor() {
+        log.info("requesting settings backend with user " + username);
         return new BasicAuthRequestInterceptor(username, password);
     }
 }
